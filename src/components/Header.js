5f8b5de6072d26f8c68aa2e4c1bff7bc68daa789
CJ Wesstrom <cjwesstrom@gmail.com>
2018-02-01 10:13:56 -0800

    moved Search.js to correct folder. Added search link to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,10 @@ class Header extends Component {
           <Link to="/" className="ml1 no-underline black">
             new
           </Link>
+          <div className="ml1">|</div>
+          <Link to="/search" className="ml1 no-underline black">
+            search
+          </Link>
           {authToken && (
             <div className="flex">
               <div className="ml1">|</div>
@@ -34,10 +38,10 @@ class Header extends Component {
               logout
             </div>
           ) : (
-            <Link to="/login" className="ml1 no-underline black">
-              login
+              <Link to="/login" className="ml1 no-underline black">
+                login
             </Link>
-          )}
+            )}
         </div>
       </div>
     )
