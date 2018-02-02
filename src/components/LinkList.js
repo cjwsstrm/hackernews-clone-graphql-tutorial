160f19f375fc06d1a976a9b8c90d8bb6aad96b65
CJ Wesstrom <cjwesstrom@gmail.com>
2018-02-01 16:22:29 -0800

    added functions _getLinksToRender, _nextPage, and _previousPage

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Link from './Link'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
+import { LINKS_PER_PAGE } from '../constants'
 
 class LinkList extends Component {
   componentDidMount() {
@@ -18,20 +19,55 @@ class LinkList extends Component {
       return <div>Error</div>
     }
 
-    const linksToRender = this.props.feedQuery.feed.links
+    const isNewPage = this.props.location.pathname.includes('new')
+    const linksToRender = this._getLinksToRender(isNewPage)
+    const page = parseInt(this.props.match.params.page, 10)
 
     return (
       <div>
-        {linksToRender.map((link, index) => (
-          <Link key={link.id} updateStoreAfterVote={this._updateCacheAfterVote} index={index} link={link}/>
-        ))}
+        <div>
+          {linksToRender.map((link, index) => (
+            <Link key={link.id} index={page ? (page - 1) * LINKS_PER_PAGE + index : index} updateStoreAfterVote={this._updateCacheAfterVote} link={link} />
+          ))}
+        </div>
+        {isNewPage &&
+        <div className='flex ml4 mv3 gray'>
+          <div className='pointer mr2' onClick={() => this._previousPage()}>Previous</div>
+          <div className='pointer' onClick={() => this._nextPage()}>Next</div>
+        </div>
+        }
       </div>
     )
   }
 
+  _getLinksToRender = (isNewPage) => {
+    if (isNewPage) {
+      return this.props.feedQuery.feed.links
+    }
+    const rankedLinks = this.props.feedQuery.feed.links.slice()
+    rankedLinks.sort((l1, l2) => l2.votes.length - l1.votes.length)
+    return rankedLinks
+  }
+
+  _nextPage = () => {
+    const page = parseInt(this.props.match.params.page, 10)
+    if (page <= this.props.feedQuery.feed.count / LINKS_PER_PAGE) {
+      const nextPage = page + 1
+      this.props.history.push(`/new/${nextPage}`)
+    }
+  }
+
+  _previousPage = () => {
+    const page = parseInt(this.props.match.params.page, 10)
+    if (page > 1) {
+      const previousPage = page - 1
+      this.props.history.push(`/new/${previousPage}`)
+    }
+  }
+
   _updateCacheAfterVote = (store, createVote, linkId) => {
     const data = store.readQuery({ query: FEED_QUERY })
-  
+
     const votedLink = data.feed.links.find(link => link.id === linkId)
     votedLink.votes = createVote.link.votes
 
