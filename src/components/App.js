93120e771a155779d473f0b9f5f3a5cbc4306449
CJ Wesstrom <cjwesstrom@gmail.com>
2018-02-01 17:02:19 -0800

    updated Route for /

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 import Header from './Header'
 import Login from './Login'
 import Search from './Search'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -13,7 +13,7 @@ class App extends Component {
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
-            <Route exact path="/" component={LinkList} />
+            <Route exact path="/" render={() => <Redirect to="/new/1" />} />
             <Route exact path="/create" component={CreateLink} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/search" component={Search} />
